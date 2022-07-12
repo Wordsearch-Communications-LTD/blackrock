ee032dcc7efe7da77d20ee66c792493ad7098514
nukeourlimo <nukeourlimo@gmail.com>
2022-07-12 11:18:13 +0100

    finishing 404 page

--- public/wp-content/themes/blackrock/templates/general/404-content.php
+++ public/wp-content/themes/blackrock/templates/general/404-content.php
@@ -3,9 +3,17 @@
     $link = get_field('link', 'options');
     $background = get_field('background_image', 'options');
 ?>
+<style>
+    .error-404 { background-image: url(<?= $background['sizes']['large'] ?>)}
+
+    @media (min-width: 1200px){
+        .error-404 { background-image: url(<?= $background['url'] ?>)}
+     }
+  </style>
+
 <section class="error-404">
     <div class="inner">
         <?= $copy ?>
-        <a href="<?= $link ?>"><?= $link ?></a>
+        <?= return_afc_link($link, 'button') ?>
     </div>
 </section>
